55a8acc29d0e6649c3211a8aee6e80c000775bf8
hitakarento <hitakarento@gmail.com>
2020-05-18 03:25:09 +0900

    wip

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,36 @@
   <div id="app">
     <h2>ワンナイト人狼DX</h2>
     <button v-if="!isConnected" @click="connect">接続</button>
+    <div v-if="nameFlag" style="display: inline-flex;">
+      <p>ログインユーザ：</p>
+      <p>{{users.length}}人</p>
+    </div>
+    <br />
+    <div v-if="nameFlag" style="display: inline-flex;">
+      <p>あなたの名前：</p>
+      <ul style="list-style: none" v-for="user in users" v-bind:key="user">
+        <li>{{user}}</li>
+      </ul>
+    </div>
     <div v-if="isConnected">
+      <div v-if="!nameFlag">
+        <input placeholder="Enter your name" v-model="name" />
+        <button @click="registerName">送信</button>
+      </div>
+      <div v-if="nameFlag">
+        <v-divider></v-divider>
+        <ul style="list-style: none" v-for="role in roles" v-bind:key="role.name">
+          <li>
+            {{role.name}}：
+            <input v-model="role.number" style="width: 0.5rem;" />人
+          </li>
+        </ul>
+        <button @click="startGame">ゲーム開始</button>
+        <v-divider></v-divider>
+      </div>
       <ul style="list-style: none" v-for="message in messages" v-bind:key="message">
         <li>{{message}}</li>
       </ul>
-      <input placeholder="Enter your message" v-model="inputMessage" />
-      <button @click="sendMessage">送信</button>
     </div>
   </div>
 </template>
@@ -22,9 +46,20 @@ export default {
   name: "App",
   data() {
     return {
+      name: "",
+      nameFlag: false,
       inputMessage: "",
       connection: null,
-      messages: []
+      messages: [],
+      users: [],
+      roles: [
+        { name: "村人", number: 1 },
+        { name: "人狼", number: 2 },
+        { name: "占い師", number: 1 },
+        { name: "怪盗", number: 1 },
+        { name: "軍人", number: 0 },
+        { name: "狂人", number: 0 }
+      ]
     };
   },
   computed: {
@@ -33,18 +68,39 @@ export default {
     }
   },
   methods: {
+    registerName() {
+      console.log(this.connection);
+
+      const data = {
+        action: "registerName",
+        data: this.name
+      };
+
+      this.nameFlag = true;
+
+      this.connection.send(JSON.stringify(data));
+    },
     sendMessage() {
       console.log("Hello");
       console.log(this.connection);
 
-      const msg = {
+      const data = {
         action: "sendmessage",
         data: this.inputMessage
       };
 
-      this.connection.send(JSON.stringify(msg));
+      this.connection.send(JSON.stringify(data));
 
       this.inputMessage = "";
+    },
+    startGame() {
+      const data = {
+        action: "startGame",
+        data: this.roles
+      };
+
+      console.log(JSON.stringify(data));
+      this.connection.send(JSON.stringify(data));
     }
   },
   created() {
@@ -54,8 +110,20 @@ export default {
     );
 
     this.connection.onmessage = event => {
-      console.log(event.data);
-      this.messages.push(event.data)
+      console.log(JSON.parse(event.data).type);
+      switch (JSON.parse(event.data).type) {
+        case "join":
+          this.messages.push(
+            JSON.parse(event.data).message + "が入室しました。"
+          );
+          this.users.push(JSON.parse(event.data).message);
+          break;
+        case "message":
+          this.messages.push(JSON.parse(event.data).message);
+          break;
+        default:
+          this.messages.push("予期せぬデータを受信しました");
+      }
     };
 
     this.connection.onopen = event => {
