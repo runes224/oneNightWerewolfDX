fa23e865fe5d1d54628180b07bf2312fc9009763
hitakarento <hitakarento@gmail.com>
2020-10-18 18:02:48 +0900

    名前登録のテストを追加

---
+++ test/src/api/registerName.test.ts
@@ -0,0 +1,101 @@
+describe('registerName', (): void => {
+  jest.setTimeout(30000);
+  const socket_1 = new WebSocket("wss://oy4l1o06be.execute-api.ap-northeast-1.amazonaws.com/prod");
+  const socket_2 = new WebSocket("wss://oy4l1o06be.execute-api.ap-northeast-1.amazonaws.com/prod");
+
+  test.concurrent('1人目の名前の登録ができること。', async () => {
+    expect.assertions(2);
+    let i = 0;
+    socket_1.onmessage = event => {
+      const receivedData = JSON.parse(event.data);
+      if (i === 0) {
+        expect(receivedData).toEqual(expectReceivedData1);
+        i++;
+      } else {
+        expect(receivedData).toEqual(expectReceivedData2);
+      }
+    };
+
+    const sendData1 = {
+      action: "registerName",
+      name: "testName1",
+      gameMasterFlag: true,
+      roomId: "12345"
+    };
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    if (1 === socket_1.readyState) {
+      socket_1.send(JSON.stringify(sendData1));
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+  });
+
+  test.concurrent('2人目の名前の登録ができること.', async () => {
+    expect.assertions(4);
+    let i = 0;
+    socket_2.onmessage = event => {
+      const receivedData = JSON.parse(event.data);
+      if (i === 0) {
+        expect(receivedData).toEqual(expectReceivedData1);
+        i++;
+      } else {
+        expect(receivedData).toEqual(expectReceivedData2);
+      }
+    }
+
+    const sendData2 = {
+      action: "registerName",
+      name: "testName2",
+      gameMasterFlag: false,
+      roomId: "12345"
+    };
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    if (1 === socket_2.readyState) {
+      socket_2.send(JSON.stringify(sendData2));
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    console.log("5秒経過");
+  });
+
+
+  test('websocket server 1 との接続を切断できること.', (done) => {
+    expect.assertions(1);
+    socket_1.onclose = event => {
+      expect(event.wasClean).toBe(true);
+      done();
+    };
+    socket_1.close();
+  });
+
+  test('websocket server 2 との接続を切断できること.', (done) => {
+    expect.assertions(1);
+    socket_2.onclose = event => {
+      expect(event.wasClean).toBe(true);
+      done();
+    };
+    socket_2.close();
+  });
+})
+
+const expectReceivedData1 = {
+  type: "join",
+  member: {
+    name: "testName1",
+    gameMasterFlag: true,
+    notExistRoomFlag: false,
+    roomId: expect.any(Number)
+  },
+  room: expect.any(Object)
+}
+
+const expectReceivedData2 = {
+  type: "join",
+  member: {
+    name: "testName2",
+    gameMasterFlag: false,
+    notExistRoomFlag: true,
+    roomId: expect.any(Number)
+  },
+  room: expect.any(Object)
+}
